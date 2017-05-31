23379610e0fc9d1e574af4fb8fce87ce4197b0e3
lightcong <758978820@qq.com>
2017-05-31 09:58:34 +0800

    替换poller，上传epoll代码待测试

--- poller.py
+++ poller.py
@@ -84,23 +84,23 @@ class SelectPoller(object):
 		del self.channel_map[channel._fd]
 
 
-
 class KqueuePoller(object):
 	'''
 	基于kqueue 的poller
 	'''
-	def __init__(self,logger):
-		import select
-		self.channel_map={}
-		self.channel_count=0
-		self._logger=logger
-		self.kq=select.kqueue()
 
-	def poll(self,timeout):
-		active_channel=[]
-		import select,error
+	def __init__(self, logger):
+		import select
+		self.channel_map = {}
+		self.channel_count = 0
+		self._logger = logger
+		self.kq = select.kqueue()
+
+	def poll(self, timeout):
+		active_channel = []
+		import select, error
 		try:
-			re_kevent_lst=self.kq.control(None,self.channel_count,timeout)
+			re_kevent_lst = self.kq.control(None, 2 * self.channel_count, timeout)  # 读写分开添加
 		except select.error, err:
 			if err.args[0] != error.EINTR:
 				# poller error
@@ -110,68 +110,69 @@ class KqueuePoller(object):
 				return active_channel
 
 		for kevent in re_kevent_lst:
-			if kevent.filter==select.KQ_FILTER_READ:
+			if kevent.filter == select.KQ_FILTER_READ:
 				# 可读
-				channel_fd=kevent.ident
-				channel_ins=self.channel_map[channel_fd]
-				channel_ins.readable=True
+				channel_fd = kevent.ident
+				channel_ins = self.channel_map[channel_fd]
+				channel_ins.readable = True
 				active_channel.append(channel_ins)
 
-			if kevent.filter==select.KQ_FILTER_WRITE:
-				#可写
-				channel_fd=kevent.ident
-				channel_ins=self.channel_map[channel_fd]
-				channel_ins.writable=True
+			if kevent.filter == select.KQ_FILTER_WRITE:
+				# 可写
+				channel_fd = kevent.ident
+				channel_ins = self.channel_map[channel_fd]
+				channel_ins.writable = True
 				active_channel.append(channel_ins)
 
 		return active_channel
 
-	def update_channel(self,channel):
+	def update_channel(self, channel):
 		'''
 		更新channel_map,同时也要更新kevent_lst
 		'''
 		import select
 		if not channel:
 			return
-		fd=channel._fd
+		fd = channel._fd
 
 		if not fd in self.channel_map:
-			#第一次添加,而不是修改
-			self.channel_count+=1
+			# 第一次添加,而不是修改
+			self.channel_count += 1
 
+		self.channel_map[fd] = channel
 
-		self.channel_map[fd]=channel
+		change_event_lst = []
+		# 读监听与写监听,要添加两次
 
-		change_event_lst=[]
-		#读监听与写监听,要添加两次
-
-		if channel.need_read==True:
+		if channel.need_read == True:
 			# 增加读监听,重复增加不会有影响
-			kevent=select.kevent(channel._fd,filter=select.KQ_FILTER_READ,flags=select.KQ_EV_ADD|select.KQ_EV_ENABLE)
+			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_READ,
+								   flags=select.KQ_EV_ADD | select.KQ_EV_ENABLE)
 			change_event_lst.append(kevent)
 		else:
 			# 删除读监听
-			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_READ,flags=select.KQ_EV_DELETE)
+			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_READ, flags=select.KQ_EV_DELETE)
 			change_event_lst.append(kevent)
 
-		if channel.need_write==True:
-			#增加写监听,重复增加不会有影响
-			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_WRITE,flags=select.KQ_EV_ADD | select.KQ_EV_ENABLE)
+		if channel.need_write == True:
+			# 增加写监听,重复增加不会有影响
+			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_WRITE,
+								   flags=select.KQ_EV_ADD | select.KQ_EV_ENABLE)
 			change_event_lst.append(kevent)
 			pass
 
 		else:
-			#删除写监听
-			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_WRITE,flags=select.KQ_EV_DELETE)
+			# 删除写监听
+			kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_WRITE, flags=select.KQ_EV_DELETE)
 			change_event_lst.append(kevent)
 
 		try:
-			self.kq.control(change_event_lst,len(change_event_lst),0)
+			self.kq.control(change_event_lst, len(change_event_lst), 0)
 		except select.error, err:
 			# 修改kqueue失败
 			raise Exception()
 
-	def remove_channel(self,channel):
+	def remove_channel(self, channel):
 		'''
 		将某个channel 从channel_map 中删除
 		同时也要更新kevent_lst
@@ -184,9 +185,9 @@ class KqueuePoller(object):
 			return
 
 		del self.channel_map[channel._fd]
-		self.channel_count=self.channel_count-1
+		self.channel_count = self.channel_count - 1
 
-		#删除读写监听
+		# 删除读写监听
 		change_event_lst = []
 		kevent = select.kevent(channel._fd, filter=select.KQ_FILTER_WRITE, flags=select.KQ_EV_DELETE)
 		change_event_lst.append(kevent)
@@ -201,33 +202,101 @@ class KqueuePoller(object):
 			raise Exception()
 
 
-
-
-
+# todo 测试epoller
+'''
 class  EpollPoller(object):
-	'''
-	基于epoll的poller
-	'''
+	#基于epoll的poller
 	def __init__(self,logger):
 		import select
 		self.channel_map={}
+		self.channel_count=0
 		self._logger=logger
-		self.epoll=select.epoll()
+		self.epoller=select.epoll()
 
 	def poll(self,timeout):
-		pass
+		active_channel = []
+		import select, error
+		try:
+			re_event_lst = self.epoller.poll(timeout,self.channel_count)
+		except select.error, err:
+			if err.args[0] != error.EINTR:
+				# poller error
+				raise Exception()
+			else:
+				# 阻塞调用被信号打断
+				return active_channel
+
+
+		for fd,event in re_event_lst:
+			#可读
+			if event&(select.EPOLLIN|select.EPOLLPRI):
+				channel_ins = self.channel_map[fd]
+				channel_ins.readable = True
+				active_channel.append(channel_ins)
+			#可写
+			if event&select.EPOLLOUT:
+				channel_ins = self.channel_map[fd]
+				channel_ins.readable = True
+				active_channel.append(channel_ins)
+			#异常
+			if event&(select.POLLHUP | select.EPOLLERR | select.EPOLLNVAL):
+				channel_ins = self.channel_map[fd]
+				channel_ins.err = True
+				active_channel.append(channel_ins)
+
+
+		return active_channel
+
+
 
 
 	def update_channel(self,channel):
+		#更新channel_map,同时也要更新监听列表
+		import select
+		if not channel:
+			return
+		fd = channel._fd
+
+		event_task=0
+		#设置时间监听选项
+		if channel.need_read == True:
+			event_task=event_task|(select.EPOLLIN|select.EPOLLPRI)
+
+		if channel.need_write == True:
+			event_task=event_task|select.EPOLLOUT
+
+
+		if not fd in self.channel_map:
+			# 第一次添加,而不是修改
+			self.channel_count += 1
+			self.epoller.register(fd,event_task)
+		else:
+			#修改
+			self.epoller.modify(fd,event_task)
+
+		self.channel_map[fd] = channel
+
 		pass
 
 
 	def remove_channel(self,channel):
-		pass
+		#将某个channel 从channel_map 中删除
+		#同时也要更新监听列表
 
+		if not channel:
+			return
 
+		if not channel._fd in self.channel_map:
+			return
 
+		del self.channel_map[channel._fd]
+		self.channel_count = self.channel_count - 1
 
+		#更新列表
+		self.epoller.unregister(channel._fd)
+
+'''
+poller = KqueuePoller
 
 if __name__ == '__main__':
 	pass
